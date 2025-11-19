bd53c7534bf066bb6ac6537b8314c3d1f7ec1233
Leonhard Breuer <git@leonhardbreuer.de>
2025-11-19 16:22:41 +0100

    feat: add basic Store that can store jsonBytes

---
+++ internal/cache/store.go
@@ -0,0 +1,42 @@
+package cache
+
+import (
+	"sync"
+
+	"github.com/goccy/go-yaml"
+)
+
+type ContentStore struct {
+	mu        sync.RWMutex
+	jsonBytes map[string][]byte
+}
+
+var store = &ContentStore{}
+
+func Get(slug string) []byte {
+	store.mu.RLock()
+	data := store.jsonBytes[slug]
+	store.mu.RUnlock()
+	return data
+}
+
+func AddAndConv(slug string, yamlData []byte) error {
+	jsonData, err := yaml.YAMLToJSON(yamlData)
+	if err != nil {
+		return err
+	}
+
+	if err := Add(slug, jsonData); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Add(slug string, jsonData []byte) error {
+	store.mu.Lock()
+	store.jsonBytes[slug] = jsonData
+	store.mu.Unlock()
+
+	return nil
+}
